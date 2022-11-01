dcac244ba2e8464bd962814da32e6bfe756db611
ElenaUJ <elena-ha@web.de>
2022-11-01 13:15:26 -0400

    Created array of Pokemon and their attributes

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,37 @@
-alert('Hello world');
-
-let favouriteFood = 'fajitas';
-document.write(favouriteFood);
+let pokemonList = [
+    {
+        name: 'Pikachu', 
+        height: 0.4, 
+        types: ['electric']
+    },
+    {
+        name: 'Mew', 
+        height: 0.4, 
+        types: ['psychic']
+    },
+    {
+        name: 'Psyduck', 
+        height: 0.8, 
+        types: ['water']
+    },
+    {
+        name: 'Jigglypuff', 
+        height: 0.5, 
+        types: ['fairy', 'normal']
+    },
+    {
+        name: 'Eevee', 
+        height: 0.3, 
+        types: ['normal']
+    },
+    {
+        name: 'Cinderace', 
+        height: 1.4, 
+        types: ['fire']
+    },
+    {
+        name: 'Charizard', 
+        height: 1.7, 
+        types: ['fire', 'flying']
+    }
+]
