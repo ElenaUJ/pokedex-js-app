d4399e9ffa84f1bd9c538c7f60801aa7ade5233b
ElenaUJ <elena-ha@web.de>
2022-11-10 11:15:35 -0500

    Clarified comment

--- js/scripts.js
+++ js/scripts.js
@@ -38,7 +38,7 @@ let pokemonList = [
 
 /* To display list of Pokemon with their respective heights in the HTML file. 
 Loop: let i = 0 initialized loop at first arrax index; i < pokemonList.length makes sure loops stops at last array-index; pokemonList[i].name refers to the respective pokemon's (object's) name (key-value)
-Condition: If Pokemon height equals or is larger than 1.5, it will be pointed out using modified string; \is used to hide single quote in "that's"  */
+Condition: If Pokemon height equals or is larger than 1.5, it will be pointed out using modified string; \is used to hide single quote in "that's" (escape character) */
 for (let i = 0; i < pokemonList.length; i++) {
     if (pokemonList[i].height >= 1.5) {
         document.write('<h2>' + pokemonList[i].name + '</h2><p> height: ' + pokemonList[i].height + ' m</p><p style="color: #CC0000">Wow, that\'s big!</p>');
