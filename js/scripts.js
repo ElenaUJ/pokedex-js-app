d1743269b43de2fcf947c2be42f2368dcf1e65f4
ElenaUJ <elena-ha@web.de>
2022-11-21 14:02:27 -0500

    Added function displaying API-fetched list in the DOM
    Added function displaying API-fetched list in the DOM

--- js/scripts.js
+++ js/scripts.js
@@ -118,9 +118,12 @@ let pokemonRepository = (function() {
 // The IIFE function is self-executing, hence why it ends with parentheses
 })();
 
-// Declaration of the function that should be looped over each array item. addListPokemon() function is called within the function declaration. Parameter of the printList() function has to be the same as the addListPokemon()'s arguments.
-function printList(pokemon) {
-    pokemonRepository.addListPokemon(pokemon);
-}
-// Calling the forEach method to run the addListPokemon function for every array item of the pokemonList array. (Has to be accessed by calling the getAll() function.) ForEach method takes functions as arguments.
-pokemonRepository.getAll().forEach(printList);
+// Implementation of all Pokemon from the external API in the app's DOM. List should only be displayed after the data is loaded from the API.
+pokemonRepository.loadList().then(function() {
+    // Now the data is loaded! Following is the callback function which has two functions as a value. First, the function which should be looped over each array item. addListPokemon() function is called within the function declaration. Parameter of the printList() function has to be the same as the addListPokemon()'s arguments.
+    function printList(pokemon) {
+        pokemonRepository.addListPokemon(pokemon);
+    }
+    // Calling the forEach method to run the addListPokemon function for every array item of the pokemonList array. (Has to be accessed by calling the getAll() function.) ForEach method takes functions as arguments.
+    pokemonRepository.getAll().forEach(printList);
+});
